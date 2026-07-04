3b8389496b24d20401d8e60cf3b35f751665ad1e
Chibueze Obioma <justixxchiobi@gmail.com>
2026-07-04 22:50:13 +0100

    Initial commit: ShelemJ website scaffold

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./*.html", "./products/*.html", "./js/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        charcoal: '#0D0D0D',
+        charcoal2: '#17140F',
+        gold: '#C9A44C',
+        goldlight: '#E3C572',
+        wine: '#6B1F2A',
+        cream: '#F3EDE3',
+        mutedgold: '#A69572',
+      },
+      fontFamily: {
+        serif: ['Playfair Display', 'serif'],
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
