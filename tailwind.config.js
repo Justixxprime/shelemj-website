00b08da3cdc5060a2d0a91194c0b358dc1e9adb8
Chibueze Obioma <justixxchiobi@gmail.com>
2026-07-05 05:55:01 +0100

    update site

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        charcoal: '#0D0D0D',
-        charcoal2: '#17140F',
-        gold: '#C9A44C',
-        goldlight: '#E3C572',
-        wine: '#6B1F2A',
-        cream: '#F3EDE3',
-        mutedgold: '#A69572',
+        charcoal: "#0D0D0D",
+        charcoal2: "#17140F",
+        gold: "#C9A44C",
+        goldlight: "#E3C572",
+        wine: "#6B1F2A",
+        cream: "#F3EDE3",
+        mutedgold: "#A69572",
       },
       fontFamily: {
-        serif: ['Playfair Display', 'serif'],
-        sans: ['Poppins', 'sans-serif'],
+        serif: ["Playfair Display", "serif"],
+        sans: ["Poppins", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
